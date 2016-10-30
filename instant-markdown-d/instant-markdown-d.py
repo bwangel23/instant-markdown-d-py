ec02cee31267f0a501249d11e4dbdf1ba7a8677e
bwangel <bwangel.me@gmail.com>
2016-10-30 15:19:47 +0800

    add index page

--- instant-markdown-d/instant-markdown-d.py
+++ instant-markdown-d/instant-markdown-d.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
+import tornado
+import tornado.web
+from tornado.ioloop import IOLoop
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+listen_ip =  '0.0.0.0' if os.environ.get('INSTANT_MARKDOWN_OPEN_TO_THE_WORLD') else '127.0.0.1'
+listen_port = os.environ.get('INSTANT_MARKDOWN_DAEMON_PORT', 8090)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
 
 
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler)
+        ]
+        settings = dict(
+            debug=True,
+            template_path=os.path.join(BASE_DIR, 'templates'),
+            static_path=os.path.join(BASE_DIR, 'static'),
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+
+def main():
+    app = Application()
+    app.listen(listen_port, listen_ip)
+    IOLoop.current().start()
+
+
+if __name__ == '__main__':
+    main()
