4bfb3f19ea077935f77b447e46d8b4f2147e0edc
bwangel <bwangel.me@gmail.com>
2016-12-14 22:37:19 +0800

    feat(test): add test for websocket

--- instant-markdown-d/instant_markdown_d.py
+++ instant-markdown-d/instant_markdown_d.py
@@ -5,21 +5,35 @@ import os
 
 import tornado
 import tornado.web
+import tornado.websocket
 from tornado.ioloop import IOLoop
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 listen_ip =  '0.0.0.0' if os.environ.get('INSTANT_MARKDOWN_OPEN_TO_THE_WORLD') else '127.0.0.1'
 listen_port = os.environ.get('INSTANT_MARKDOWN_DAEMON_PORT', 8090)
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
 
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/", MainHandler)
+            (r"/", MainHandler),
+            (r"/websocket", WebSocketHandler),
         ]
         settings = dict(
             debug=True,
---
+++ instant-markdown-d/test_instant_markdown_d.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tornado
+import tornado.testing
+import tornado.websocket
+
+from instant_markdown_d import Application
+
+class WebSocketTestCase(tornado.testing.AsyncHTTPTestCase):
+    def get_app(self):
+        app = Application()
+        return app
+
+    @tornado.testing.gen_test
+    def test_websocket_connect(self):
+        ws_url = "ws://localhost:{0}/websocket".format(self.get_http_port())
+        ws_client = yield tornado.websocket.websocket_connect(ws_url)
+
+        message = "Hi, This is the message"
+        ws_client.write_message(message)
+        response = yield ws_client.read_message()
+        self.assertEqual(response, message)
+
